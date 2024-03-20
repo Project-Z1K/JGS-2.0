544fc9d80bf088b301750726da155ca7c92d5274
Volcano-MP <149071205+Project-Z1K@users.noreply.github.com>
2024-03-20 15:35:59 -0700

    started imgui

--- JGS/dllmain.cpp
+++ JGS/dllmain.cpp
@@ -29,6 +29,27 @@ using namespace SDK;
 
 #include "Hooks.h"
 
+#include "imgui.h"
+#include "imgui_impl_dx9.h"
+#include "imgui_impl_win32.h"
+#include <d3d9.h>
+#include <tchar.h>
+#include <thread>
+
+#pragma comment(lib, "d3d9.lib")
+
+// Data
+static LPDIRECT3D9              g_pD3D = NULL;
+static LPDIRECT3DDEVICE9        g_pd3dDevice = NULL;
+static D3DPRESENT_PARAMETERS    g_d3dpp = {};
+
+// Forward declarations of helper functions
+bool CreateDeviceD3D(HWND hWnd);
+void CleanupDeviceD3D();
+void ResetDevice();
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+
 DWORD WINAPI MainThread(LPVOID)
 {
     Util::InitConsole();
